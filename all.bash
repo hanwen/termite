1495a5d559a247276b2d1256b02f393c3e34583d
Han-Wen Nienhuys <hanwen@google.com>
2011-07-01 01:28:01 -0300

    add tool to all.bash

--- all.bash
+++ all.bash
@@ -4,7 +4,7 @@ set -eux
 
 for d in rpcfs \
     termite/chroot termite/worker termite/fsserver \
-    termite/rpcfs termite/master ; \
+    termite/rpcfs termite/master termite/tool ; \
 do
   gomake -C $d "$@"
 done
