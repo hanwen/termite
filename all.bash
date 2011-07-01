3e3a6717b2e5246809e79785ccd75c879354833b
Han-Wen Nienhuys <hanwen@google.com>
2011-07-01 14:05:15 -0300

    Move binaries to bin/

--- all.bash
+++ all.bash
@@ -3,8 +3,8 @@
 set -eux
 
 for d in rpcfs \
-    termite/chroot termite/worker termite/fsserver \
-    termite/rpcfs termite/master termite/tool ; \
+    bin/chroot bin/worker bin/fsserver \
+    bin/rpcfs bin/master bin/tool ; \
 do
   gomake -C $d "$@"
 done
