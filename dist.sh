18196f100420f58bc9819b62609dc99a3a99b0f1
Han-Wen Nienhuys <hanwen@google.com>
2011-07-13 01:35:07 -0300

    Add script to distribute binaries.

---
+++ dist.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -eux
+rm -rf termite-bin
+mkdir termite-bin
+
+for x in \
+    bin/coordinator bin/chroot bin/worker bin/master bin/tool ;
+do
+    gomake -C $x
+    cp $x/$(basename $x) termite-bin/
+done 
+
+tar cjf termite-bin.tar.bz2 termite-bin/
