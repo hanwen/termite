abb796dfaf57b1db6116042a96cc06a0613cf7fb
Han-Wen Nienhuys <hanwen@google.com>
2011-07-14 23:03:18 -0300

    Use wrapper name in dist.sh as well.

--- dist.sh
+++ dist.sh
@@ -5,7 +5,7 @@ rm -rf termite-bin
 mkdir termite-bin
 
 for x in \
-    bin/coordinator bin/chroot bin/worker bin/master bin/tool ;
+    bin/coordinator bin/chroot bin/worker bin/master bin/wrapper ;
 do
     gomake -C $x
     cp $x/$(basename $x) termite-bin/
