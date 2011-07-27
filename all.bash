90e6dd3f3aca96e7d979a4bd3793d3855c9ccc38
Han-Wen Nienhuys <hanwen@google.com>
2011-07-26 23:08:01 -0300

    Add coordinator to all.bash

--- all.bash
+++ all.bash
@@ -5,6 +5,7 @@ set -eux
 rm -f termite/version.gen.go
 
 for d in termite \
+    bin/coordinator \
     bin/chroot bin/worker bin/fsserver \
     bin/rpcfs bin/master bin/wrapper bin/shell-wrapper ; \
 do
