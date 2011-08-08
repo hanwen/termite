600090f13823a27abd1854e864b677db25b279f0
Han-Wen Nienhuys <hanwen@google.com>
2011-08-08 02:28:47 -0300

    Add listfiles.go and test.

---
+++ termite/listfiles.go
@@ -0,0 +1,22 @@
+package termite
+import (
+	"os"
+	"path/filepath"
+)
+
+type fileLister map[string]os.FileInfo 
+func (me *fileLister) VisitFile(path string, f *os.FileInfo) {
+	(*me)[path] = *f
+}
+
+func (me *fileLister) VisitDir(path string, f *os.FileInfo) bool {
+	(*me)[path] = *f
+	return true
+}
+	
+func ListFilesRecursively(root string) map[string]os.FileInfo {
+	l := fileLister{}
+	filepath.Walk(root, &l, nil)
+	return l
+}
+
---
+++ termite/listfiles_test.go
@@ -0,0 +1,24 @@
+package termite
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func TestListFilesRecursively(t *testing.T) {
+	dir, _ := ioutil.TempDir("", "")
+	ioutil.WriteFile(dir + "/foo", []byte{42}, 0644)
+	entries := ListFilesRecursively(dir)
+	if len(entries) != 2 {
+		t.Errorf("expect 2 entries %#v", entries)
+	}
+	attr, ok := entries[dir]
+	if !ok || !attr.IsDirectory() {
+		t.Errorf("unexpected entry for 'dir': %v, %#v", ok, entries[dir])
+	}
+
+	attr, ok = entries[dir + "/foo"]
+	if !ok || !attr.IsRegular() {
+		t.Errorf("unexpected entry for 'dir/foo': %v, %#v", ok, entries[dir])
+	}
+}
