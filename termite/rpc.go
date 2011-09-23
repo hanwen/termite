b3f4f50058212754bffbf2c9ef8ba3fe394afac5
Han-Wen Nienhuys <hanwen@google.com>
2011-09-23 15:38:32 -0300

    Add missing rpc.go

---
+++ termite/rpc.go
@@ -0,0 +1,113 @@
+package termite
+
+import (
+	"fmt"
+	"github.com/hanwen/go-fuse/fuse"	
+	"os"
+)
+
+type ContentRequest struct {
+	Hash       string
+	Start, End int
+}
+
+func (me *ContentRequest) String() string {
+	return fmt.Sprintf("%x [%d, %d]", me.Hash, me.Start, me.End)
+}
+
+type ContentResponse struct {
+	Chunk []byte
+}
+ 
+type AttrRequest struct {
+	Name string
+}
+
+type FileAttr struct {
+	Path string
+	*os.FileInfo
+	fuse.Status
+	Hash    string
+	Link    string
+}
+
+type AttrResponse struct {
+	Attrs    []*FileAttr
+}
+ 
+ 
+type DirRequest struct {
+	Name string
+}
+
+type DirResponse struct {
+	NameModeMap map[string]uint32
+}
+ 
+type UpdateRequest struct {
+	Files []*FileAttr
+}
+
+type UpdateResponse struct {
+
+}
+ 
+type MirrorStatusRequest struct {
+
+}
+
+type MirrorStatusResponse struct {
+	Root         string
+	Granted      int
+	Running      []string
+	ShuttingDown bool
+	WaitingTasks int
+	IdleFses     int
+}
+ 
+type WorkerStatusRequest struct {
+ 
+}
+
+type WorkerStatusResponse struct {
+	MirrorStatus []MirrorStatusResponse
+	Version      string
+	MaxJobCount  int
+	ShuttingDown bool
+}
+ 
+type WorkResponse struct {
+	Exit   os.Waitmsg
+	Files  []*FileAttr
+	Stderr string
+	Stdout string
+}
+
+type WorkRequest struct {
+	Prefetch []*FileAttr
+
+	// Id of connection streaming stdin.
+	StdinId      string
+	Debug        bool
+	WritableRoot string
+	Binary       string
+	Argv         []string
+	Env          []string
+	Dir          string
+	RanLocally   bool
+}
+ 
+func (me *WorkRequest) Summary() string {
+	return fmt.Sprintf("stdin %s cmd %s", me.StdinId, me.Argv)
+}
+type CreateMirrorRequest struct {
+	RpcId        string
+	RevRpcId     string
+	WritableRoot string
+	// Max number of processes to reserve.
+	MaxJobCount int
+}
+
+type CreateMirrorResponse struct {
+	GrantedJobCount int
+}
