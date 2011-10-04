4c8ee629f9284ece800540a5686a6be6980a5ad9
Han-Wen Nienhuys <hanwen@google.com>
2011-10-03 23:29:36 -0300

    Tune e2e test.

--- test/test.sh
+++ test/test.sh
@@ -11,13 +11,13 @@ dd if=/dev/random of=secret.txt bs=20 count=1 && chmod 0600 secret.txt
 ${TERMITE_DIR}/bin/coordinator/coordinator -port ${pprefix}8 &
 
 sudo -n true
-for w in 1 2 3 4
+for w in 1 2 
 do
   rm -f w$w.log w$w.stderr
   sudo -b ${TERMITE_DIR}/bin/worker/worker -coordinator ${coord} -port ${pprefix}$w -jobs 4 -logfile w$w.log &> w$w.stderr
 done
 sleep 1
-${TERMITE_DIR}/bin/master/master -socket ${name}/.termite-socket -jobs 50 -port ${pprefix}9 -coordinator ${coord} -exclude usr/lib/locale/locale-archive >& master.log &
+${TERMITE_DIR}/bin/master/master -socket ${name}/.termite-socket -jobs 50 -port ${pprefix}9 -coordinator ${coord} >& master.log &
 sleep 1
 
 ( export PATH="${TERMITE_DIR}/bin/shell-wrapper:$PATH";
