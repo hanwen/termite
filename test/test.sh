89e870dc469ca974f5b26b579574a789e032a0d2
Han-Wen Nienhuys <hanwen@google.com>
2011-10-20 23:54:38 -0700

    Allow parameter tuning in test.

--- test/test.sh
+++ test/test.sh
@@ -11,14 +11,23 @@ dd if=/dev/urandom of=secret.txt bs=20 count=1 && chmod 0600 secret.txt
 ${TERMITE_DIR}/bin/coordinator/coordinator -port ${pprefix}8 &
 sleep 1
 sudo -n true
-for w in 1 2 
+
+workers=2
+workerjobs=2
+masterjobs=8
+if test -f $name/params.sh
+then
+  . $name/params.sh
+fi
+
+for w in $(seq 1 $workers)
 do
   rm -f w$w.log w$w.stderr
-  sudo -b ${TERMITE_DIR}/bin/worker/worker -coordinator ${coord} -port ${pprefix}$w -jobs 4 -logfile w$w.log &> w$w.stderr
+  sudo -b ${TERMITE_DIR}/bin/worker/worker -coordinator ${coord} -port ${pprefix}$w -jobs $workerjobs -logfile w$w.log &> w$w.stderr
 done
 sleep 1
 rm master.log
-${TERMITE_DIR}/bin/master/master -secret secret.txt -socket ${name}/.termite-socket -jobs 50 -port ${pprefix}9 -coordinator ${coord} >& master.log &
+${TERMITE_DIR}/bin/master/master -secret secret.txt -socket ${name}/.termite-socket -jobs $masterjobs -port ${pprefix}9 -coordinator ${coord} >& master.log &
 sleep 1
 
 ( export PATH="${TERMITE_DIR}/bin/shell-wrapper:$PATH";
