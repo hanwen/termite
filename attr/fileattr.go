105d586809eda83e2f70f0b6db1b38c2c45aef7b
Han-Wen Nienhuys <hanwen@google.com>
2011-12-12 13:13:11 -0200

    Bail out if Attr == nil in WriteXAttr.

--- attr/fileattr.go
+++ attr/fileattr.go
@@ -77,10 +77,10 @@ func (me FileAttr) Copy(withdir bool) *FileAttr {
 const _TERM_XATTR = "user.termattr"
 
 type EncodedAttr struct {
-	Perm uint16
-	Nlink uint16
-	Size uint64
-	Ino  uint64
+	Perm    uint16
+	Nlink   uint16
+	Size    uint64
+	Ino     uint64
 	Mtimens uint64
 
 	// total: 28 bytes.
@@ -92,8 +92,8 @@ func (e *EncodedAttr) FromAttr(a *fuse.Attr) {
 	e.Perm = uint16(a.Mode & 07777)
 	e.Nlink = uint16(a.Nlink)
 	e.Size = a.Size
-	e.Ino  = a.Ino
-	e.Mtimens = 1e9 * a.Mtime + uint64(a.Mtimensec)
+	e.Ino = a.Ino
+	e.Mtimens = 1e9*a.Mtime + uint64(a.Mtimensec)
 }
 
 func (e *EncodedAttr) Eq(b *EncodedAttr) bool {
@@ -136,7 +136,7 @@ func (e *EncodedAttr) Encode(h string) []byte {
 		byte(e.Mtimens >> 56),
 	}
 
-	b := make([]byte, len(a) + len(h))
+	b := make([]byte, len(a)+len(h))
 	copy(b, a)
 	copy(b[len(a):], h)
 	return b
@@ -146,31 +146,35 @@ func (e *EncodedAttr) Decode(in []byte) (hash []byte) {
 	if uintptr(len(in)) < sizeEncodedAttr {
 		return nil
 	}
-	e.Perm = uint16(in[0]) | uint16(in[1]) << 8
+	e.Perm = uint16(in[0]) | uint16(in[1])<<8
 	in = in[2:]
-	e.Nlink = uint16(in[0]) | uint16(in[1]) << 8
+	e.Nlink = uint16(in[0]) | uint16(in[1])<<8
 	in = in[2:]
-	e.Size = uint64(in[0]) | uint64(in[1]) << 8 | uint64(in[2]) << 16 | uint64(in[3]) << 24 | uint64(in[4]) << 32|uint64(in[5]) << 40|uint64(in[6]) << 48|uint64(in[7]) << 56
+	e.Size = uint64(in[0]) | uint64(in[1])<<8 | uint64(in[2])<<16 | uint64(in[3])<<24 | uint64(in[4])<<32 | uint64(in[5])<<40 | uint64(in[6])<<48 | uint64(in[7])<<56
 	in = in[8:]
-	e.Ino = uint64(in[0]) | uint64(in[1]) << 8 | uint64(in[2]) << 16 | uint64(in[3]) << 24 | uint64(in[4]) << 32|uint64(in[5]) << 40|uint64(in[6]) << 48|uint64(in[7]) << 56
+	e.Ino = uint64(in[0]) | uint64(in[1])<<8 | uint64(in[2])<<16 | uint64(in[3])<<24 | uint64(in[4])<<32 | uint64(in[5])<<40 | uint64(in[6])<<48 | uint64(in[7])<<56
 	in = in[8:]
-	e.Mtimens = uint64(in[0]) | uint64(in[1]) << 8 | uint64(in[2]) << 16 | uint64(in[3]) << 24 | uint64(in[4]) << 32|uint64(in[5]) << 40|uint64(in[6]) << 48|uint64(in[7]) << 56
+	e.Mtimens = uint64(in[0]) | uint64(in[1])<<8 | uint64(in[2])<<16 | uint64(in[3])<<24 | uint64(in[4])<<32 | uint64(in[5])<<40 | uint64(in[6])<<48 | uint64(in[7])<<56
 	in = in[8:]
 
 	return in
 }
 
 func (a *FileAttr) WriteXAttr(p string) {
+	if a.Attr == nil {
+		return
+	}
+
 	var e EncodedAttr
 	e.FromAttr(a.Attr)
-	
+
 	b := e.Encode(a.Hash)
 	errno := fuse.Setxattr(p, _TERM_XATTR, b, 0)
 	if errno != 0 {
 		log.Printf("Setxattr %s: code %v", p, syscall.Errno(errno))
 	}
 }
-	
+
 func (e *EncodedAttr) ReadXAttr(path string) (hash []byte) {
 	val, errno := fuse.GetXAttr(path, _TERM_XATTR)
 	if errno == 0 {
@@ -247,27 +251,42 @@ func (me *FileAttr) Merge(r FileAttr) {
 }
 
 func FuseAttrEq(a *fuse.Attr, b *fuse.Attr) bool {
-	return (a.Mode == b.Mode && a.Size == b.Size && a.Blocks == b.Blocks && a.Mtime == b.Mtime && a.Ctime == b.Ctime &&
-		a.Mtimensec == b.Mtimensec && a.Ctimensec == b.Ctimensec && a.Uid == b.Uid && a.Gid == b.Gid && a.Blksize == b.Blksize)
+	return (a.Mode == b.Mode && a.Size == b.Size && a.Mtime == b.Mtime &&
+		a.Nlink == b.Nlink &&
+		a.Ino == b.Ino && a.Mtimensec == b.Mtimensec && a.Uid == b.Uid &&
+		a.Gid == b.Gid)
 }
 
-func (me *FileAttr) IsFifo() bool { return me.Attr != nil && (uint32(me.Mode) & syscall.S_IFMT) == syscall.S_IFIFO }
+func (me *FileAttr) IsFifo() bool {
+	return me.Attr != nil && (uint32(me.Mode)&syscall.S_IFMT) == syscall.S_IFIFO
+}
 
 // IsChar reports whether the FileInfo describes a character special file.
-func (me *FileAttr) IsChar() bool { return me.Attr != nil &&  (uint32(me.Mode) & syscall.S_IFMT) == syscall.S_IFCHR }
+func (me *FileAttr) IsChar() bool {
+	return me.Attr != nil && (uint32(me.Mode)&syscall.S_IFMT) == syscall.S_IFCHR
+}
 
 // IsDir reports whether the FileInfo describes a directory.
-func (me *FileAttr) IsDir() bool { return me.Attr != nil &&  (uint32(me.Mode) & syscall.S_IFMT) == syscall.S_IFDIR }
+func (me *FileAttr) IsDir() bool {
+	return me.Attr != nil && (uint32(me.Mode)&syscall.S_IFMT) == syscall.S_IFDIR
+}
 
 // IsBlock reports whether the FileInfo describes a block special file.
-func (me *FileAttr) IsBlock() bool { return me.Attr != nil &&  (uint32(me.Mode) & syscall.S_IFMT) == syscall.S_IFBLK }
+func (me *FileAttr) IsBlock() bool {
+	return me.Attr != nil && (uint32(me.Mode)&syscall.S_IFMT) == syscall.S_IFBLK
+}
 
 // IsRegular reports whether the FileInfo describes a regular file.
-func (me *FileAttr) IsRegular() bool { return me.Attr != nil &&  (uint32(me.Mode) & syscall.S_IFMT) == syscall.S_IFREG }
+func (me *FileAttr) IsRegular() bool {
+	return me.Attr != nil && (uint32(me.Mode)&syscall.S_IFMT) == syscall.S_IFREG
+}
 
 // IsSymlink reports whether the FileInfo describes a symbolic link.
-func (me *FileAttr) IsSymlink() bool { return me.Attr != nil &&  (uint32(me.Mode) & syscall.S_IFMT) == syscall.S_IFLNK }
+func (me *FileAttr) IsSymlink() bool {
+	return me.Attr != nil && (uint32(me.Mode)&syscall.S_IFMT) == syscall.S_IFLNK
+}
 
 // IsSocket reports whether the FileInfo describes a socket.
-func (me *FileAttr) IsSocket() bool { return me.Attr != nil &&  (uint32(me.Mode) & syscall.S_IFMT) == syscall.S_IFSOCK }
-
+func (me *FileAttr) IsSocket() bool {
+	return me.Attr != nil && (uint32(me.Mode)&syscall.S_IFMT) == syscall.S_IFSOCK
+}
