e1a77818fa05e322e53493f87e8cba8c9f37f857
Han-Wen Nienhuys <hanwen@google.com>
2012-01-02 02:13:32 -0200

    New file: pair.go

---
+++ splice/pair.go
@@ -0,0 +1,77 @@
+package splice
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"syscall"
+)
+
+type Pair struct {
+	r, w *os.File
+	size int
+}
+
+func (p *Pair) MaxGrow() {
+	for p.Grow(2 * p.size) {
+	}
+}
+
+func (p *Pair) Grow(n int) bool {
+	if n > getPipeMaxSize() {
+		return false
+	}
+	if n <= p.size {
+		return true
+	}
+	newsize := p.size
+	for newsize < n {
+		newsize *= 2
+	}
+
+	newsize, errNo := fcntl(p.r.Fd(), F_SETPIPE_SZ, newsize)
+	if errNo != 0 {
+		log.Println(os.NewSyscallError("fnct", errNo))
+		return false
+	}
+	p.size = newsize
+	return true
+}
+
+func (p *Pair) Cap() int {
+	return p.size
+}
+
+func (p *Pair) Close() error {
+	err1 := p.r.Close()
+	err2 := p.w.Close()
+	if err1 != nil {
+		return err1
+	}
+	return err2
+}
+
+func (p *Pair) Read(d []byte) (n int, err error) {
+	return p.r.Read(d)
+}
+
+func (p *Pair) LoadFrom(fd int, sz int) (n int, err error) {
+	if sz > p.size {
+		return 0, fmt.Errorf("LoadFrom: not enough space %d, %d",
+			sz, p.size)
+	}
+	
+	n, err = syscall.Splice(fd, nil, p.w.Fd(), nil, sz, 0)
+	if err != nil {
+		err = os.NewSyscallError("Splice load from", err)
+	}
+	return 
+}
+
+func (p *Pair) WriteTo(fd int, n int) (m int, err error) {
+	m, err = syscall.Splice(p.r.Fd(), nil, fd, nil, int(n), 0)
+	if err != nil {
+		err = os.NewSyscallError("Splice write to: ", err)
+	}
+	return
+}
--- splice/splice.go
+++ splice/splice.go
@@ -12,10 +12,6 @@ import (
 
 var _ = log.Println
 
-type Pair struct {
-	r, w *os.File
-	size int
-}
 
 var pipeMaxSize *int
 
@@ -51,70 +47,6 @@ func fcntl(fd int, cmd int, arg int) (val int, errno syscall.Errno) {
 const F_SETPIPE_SZ = 1031
 const F_GETPIPE_SZ = 1032
 
-func (me *Pair) MaxGrow() {
-	for me.Grow(2 * me.size) {
-	}
-}
-
-func (me *Pair) Grow(n int) bool {
-	if n > getPipeMaxSize() {
-		return false
-	}
-	if n <= me.size {
-		return true
-	}
-	newsize := me.size
-	for newsize < n {
-		newsize *= 2
-	}
-
-	newsize, errNo := fcntl(me.r.Fd(), F_SETPIPE_SZ, newsize)
-	if errNo != 0 {
-		log.Println(os.NewSyscallError("fnct", errNo))
-		return false
-	}
-	me.size = newsize
-	return true
-}
-
-func (p *Pair) Cap() int {
-	return p.size
-}
-
-func (me *Pair) Close() error {
-	err1 := me.r.Close()
-	err2 := me.w.Close()
-	if err1 != nil {
-		return err1
-	}
-	return err2
-}
-
-func (p *Pair) Read(d []byte) (n int, err error) {
-	return p.r.Read(d)
-}
-
-func (p *Pair) LoadFrom(fd int, sz int) (n int, err error) {
-	if sz > p.size {
-		return 0, fmt.Errorf("LoadFrom: not enough space %d, %d",
-			sz, p.size)
-	}
-	
-	n, err = syscall.Splice(fd, nil, p.w.Fd(), nil, sz, 0)
-	if err != nil {
-		err = os.NewSyscallError("Splice load from", err)
-	}
-	return 
-}
-
-func (p *Pair) WriteTo(fd int, n int) (m int, err error) {
-	m, err = syscall.Splice(p.r.Fd(), nil, fd, nil, int(n), 0)
-	if err != nil {
-		err = os.NewSyscallError("Splice write to: ", err)
-	}
-	return
-}
-
 func newSplicePair() (me *Pair, err error) {
 	me = &Pair{}
 	me.r, me.w, err = os.Pipe()
@@ -123,15 +55,12 @@ func newSplicePair() (me *Pair, err error) {
 	}
 
 	errNo := syscall.Errno(0)
-	_, errNo = fcntl(me.r.Fd(), syscall.F_SETFL, os.O_NONBLOCK)
-	if errNo != 0 {
-		me.Close()
-		return nil, os.NewSyscallError("fcntl setfl r", errNo)
-	}
-	_, errNo = fcntl(me.w.Fd(), syscall.F_SETFL, os.O_NONBLOCK)
-	if errNo != 0 {
-		me.Close()
-		return nil, os.NewSyscallError("fcntl setfl w", errNo)
+	for _, f := range []*os.File{me.r, me.w} {
+		_, errNo = fcntl(f.Fd(), syscall.F_SETFL, os.O_NONBLOCK)
+		if errNo != 0 {
+			me.Close()
+			return nil, os.NewSyscallError("fcntl setfl", errNo)
+		}
 	}
 
 	me.size, errNo = fcntl(me.r.Fd(), F_GETPIPE_SZ, 0)
