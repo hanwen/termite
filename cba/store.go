bb011708fceddb175cd5dd85f0463b518b186926
Han-Wen Nienhuys <hanwen@google.com>
2012-06-23 18:38:48 -0300

    Split off HashWriter in separate file.

--- cba/store.go
+++ cba/store.go
@@ -4,12 +4,10 @@ import (
 	"crypto"
 	md5pkg "crypto/md5"
 	"crypto/sha1"
-	"hash"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
 	"sync"
 	"time"
 
@@ -171,71 +169,6 @@ func (store *Store) NewHashWriter() *HashWriter {
 	st.dest = tmp
 	st.hasher = store.Options.Hash.New()
 	return st
-}
-
-type HashWriter struct {
-	start  time.Time
-	hasher hash.Hash
-	dest   *os.File
-	cache  *Store
-	size   int
-}
-
-func (st *HashWriter) Sum() string {
-	return string(st.hasher.Sum(nil))
-}
-
-func (st *HashWriter) Write(p []byte) (n int, err error) {
-	n, err = st.dest.Write(p)
-	st.hasher.Write(p[:n])
-	st.size += n
-	return n, err
-}
-
-func (st *HashWriter) WriteClose(p []byte) (err error) {
-	_, err = st.Write(p)
-	if err != nil {
-		return err
-	}
-	err = st.Close()
-	return err
-}
-
-func (st *HashWriter) CopyClose(input io.Reader, size int64) error {
-	_, err := io.CopyN(st, input, size)
-	if err != nil {
-		return err
-	}
-	err = st.Close()
-	return err
-}
-
-func (st *HashWriter) Close() error {
-	st.dest.Chmod(0444)
-	err := st.dest.Close()
-	if err != nil {
-		return err
-	}
-	src := st.dest.Name()
-	dir, _ := filepath.Split(src)
-	sum := st.Sum()
-	sumpath := HashPath(dir, sum)
-
-	log.Printf("saving hash %x\n", sum)
-	err = os.Rename(src, sumpath)
-	if err != nil {
-		log.Fatal("Rename failed", err)
-	}
-
-	st.cache.mutex.Lock()
-	st.cache.have[sum] = true
-	st.cache.mutex.Unlock()
-
-	dt := time.Now().Sub(st.start)
-
-	st.cache.AddTiming("Save", st.size, dt)
-
-	return err
 }
 
 const _BUFSIZE = 32 * 1024
