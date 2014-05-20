9318a6a45d4cf44bc7925cff8f5680f38892f548
Han-Wen Nienhuys <hanwen@google.com>
2014-05-20 22:26:19 +0200

    Analyze .dep files.

--- analyze/read.go
+++ analyze/read.go
@@ -1,11 +1,13 @@
 package analyze
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"regexp"
 	"sort"
 	"strings"
 	"time"
@@ -49,7 +51,33 @@ func (d *dupWrite) HTML(g *Graph) string {
 	return fmt.Sprintf("file %q written by <ul>%s</ul>", d.write, s)
 }
 
-func ReadDir(dir string) ([]*Command, error) {
+/* parse a "target: dep" file. */
+func ParseDepFile(content []byte) ([]string, []string) {
+	content = bytes.Replace(content, []byte("\\\n"), nil, -1)
+	components := bytes.Split(content, []byte(":"))
+	if len(components) != 2 {
+		return nil, nil
+	}
+
+	targetStrs := bytes.Split(components[0], []byte(" "))
+	depStrs := bytes.Split(components[1], []byte(" "))
+
+	var targets, deps []string
+	for _, t := range targetStrs {
+		if len(t) > 0 {
+			targets = append(targets, string(t))
+		}
+	}
+	for _, d := range depStrs {
+		if len(d) > 0 {
+			deps = append(deps, string(d))
+		}
+	}
+
+	return targets, deps
+}
+
+func ReadDir(dir string, depRe *regexp.Regexp) ([]*Command, error) {
 	dir = filepath.Clean(dir)
 	result := []*Command{}
 	f, err := os.Open(dir)
@@ -80,6 +108,37 @@ func ReadDir(dir string) ([]*Command, error) {
 		}
 		a.Target = filepath.Clean(a.Target)
 		a.Filename = nm
+
+		// add contents of .dep file to the command.
+		for _, w := range a.Writes {
+			if depRe == nil || !depRe.MatchString(w) {
+				continue
+			}
+			c, err := ioutil.ReadFile(filepath.Join(base, w))
+			if err != nil {
+				continue
+			}
+
+			found := false
+			targets, deps := ParseDepFile(c)
+			if len(targets) == 0 {
+				continue
+			}
+			for _, t := range targets {
+				t = filepath.Clean(filepath.Join(a.Dir, t))
+				if t == filepath.Join(base, a.Target) {
+					found = true
+				}
+			}
+			if !found {
+				continue
+			}
+
+			for _, d := range deps {
+				a.Deps = append(a.Deps, d)
+			}
+		}
+
 		var clean []string
 		for _, p := range a.Deps {
 			p, err = filepath.Rel(base, filepath.Join(a.Dir, p))
@@ -89,6 +148,7 @@ func ReadDir(dir string) ([]*Command, error) {
 
 			clean = append(clean, p)
 		}
+
 		a.Deps = clean
 
 		result = append(result, &a)
--- bin/analyze/main.go
+++ bin/analyze/main.go
@@ -3,16 +3,22 @@ package main
 import (
 	"flag"
 	"log"
+	"regexp"
 
 	"termite/analyze"
 )
 
 func main() {
 	addr := flag.String("addr", ":8080", "address to serve on")
+	depReStr := flag.String("dep_re", "", "file name regexp for dependency files")
 	flag.Parse()
 	dir := flag.Arg(0)
 
-	results, err := analyze.ReadDir(dir)
+	var re *regexp.Regexp
+	if *depReStr != "" {
+		re = regexp.MustCompile(*depReStr)
+	}
+	results, err := analyze.ReadDir(dir, re)
 	if err != nil {
 		log.Fatal(err)
 	}
