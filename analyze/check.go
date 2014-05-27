73b8e726ce84a72b1beea6c8e1dfc4156637d4f2
Han-Wen Nienhuys <hanwen@google.com>
2014-05-28 00:23:29 +0200

    Parallelize checkTargets.

--- analyze/check.go
+++ analyze/check.go
@@ -29,7 +29,12 @@ func (u *unusedDep) HTML(g *Graph) string {
 		g.targetURL(u.Target), u.Dep)
 }
 
-func (g *Graph) checkTarget(target *Target) {
+type checkTargetResult struct {
+	errors []Error
+	edges  map[edge]struct{}
+}
+
+func (g *Graph) checkTarget(target *Target) checkTargetResult {
 	realDeps := map[*Target]*String{}
 	for r := range target.Reads {
 		t := g.TargetByWrite[r]
@@ -37,11 +42,14 @@ func (g *Graph) checkTarget(target *Target) {
 			realDeps[t] = r
 		}
 	}
+	result := checkTargetResult{
+		edges: make(map[edge]struct{}),
+	}
 
 	usedDeps := targetSet{}
 	for dep, name := range realDeps {
 		if _, ok := target.Deps[name]; ok {
-			g.UsedEdges[edge{target, dep}] = yes
+			result.edges[edge{target, dep}] = yes
 			usedDeps[dep] = yes
 		}
 		if _, ok := target.Deps[dep.Name]; ok {
@@ -56,14 +64,16 @@ func (g *Graph) checkTarget(target *Target) {
 		path := g.findPath([]*Target{target}, d)
 		if path != nil {
 			for i := 1; i < len(path); i++ {
-				g.UsedEdges[edge{path[i-1], path[i]}] = yes
+				result.edges[edge{path[i-1], path[i]}] = yes
 			}
 		} else {
 			e := &undeclaredDep{target.Name, realDeps[d]}
-			g.Errors = append(g.Errors, e)
+			result.errors = append(result.errors, e)
 			target.Errors = append(target.Errors, e)
 		}
 	}
+
+	return result
 }
 
 // finds a path reaching needle from the partial path given. Returns
@@ -96,9 +106,21 @@ nextDep:
 
 func (g *Graph) checkTargets() {
 	log.Println("checking targets")
+	results := make(chan checkTargetResult, 100)
 	for _, target := range g.TargetByName {
-		g.checkTarget(target)
+		go func(t *Target) {
+			results <- g.checkTarget(t)
+		}(target)
 	}
+
+	for _ = range g.TargetByName {
+		r := <-results
+		for e := range r.edges {
+			g.UsedEdges[e] = yes
+		}
+		g.Errors = append(g.Errors, r.errors...)
+	}
+
 	for _, target := range g.TargetByName {
 		g.checkUnusedDeps(target)
 	}
